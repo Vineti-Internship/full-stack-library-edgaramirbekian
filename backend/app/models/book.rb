c74d52b67ff95c0aca055be4dcdf4dc82b79fb82
Edgar <amirbekianedgar@gmail.com>
2018-11-27 20:23:25 +0400

    create project

--- backend/app/models/book.rb
+++ backend/app/models/book.rb
@@ -1,8 +1,8 @@
-class Book < ApplicationRecord
-  # belongs_to :writer
+# class Book < ApplicationRecord
+#   # belongs_to :writer
 
-  # validates :title, :category, :rating, :writer_id, presence: true
-  # validates :title, :uniqueness => true
-  # validates_numericality_of :rating, numericality: { only_integer: true }, :less_than_or_equal_to => 10
+#   # validates :title, :category, :rating, :writer_id, presence: true
+#   # validates :title, :uniqueness => true
+#   # validates_numericality_of :rating, numericality: { only_integer: true }, :less_than_or_equal_to => 10
 
-end
+# end
