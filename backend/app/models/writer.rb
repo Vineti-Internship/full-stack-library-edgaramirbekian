d62f8869b1cdfa994cc84af8eea9522f8bde52b4
Edgar <amirbekianedgar@gmail.com>
2018-11-29 02:00:44 +0400

    add devise to writers for authentication

--- backend/app/models/writer.rb
+++ backend/app/models/writer.rb
@@ -1,4 +1,8 @@
 class Writer < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable, :registerable,
+            :recoverable, :rememberable, :validatable
     has_many :books
 
     validates :first_name, :last_name, presence: true
