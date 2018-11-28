c758bc1ae7972b60aac42bbe88ec2127d334b764
Edgar <amirbekianedgar@gmail.com>
2018-11-29 01:21:22 +0400

    add page body template

---
+++ frontend/src/components/shop.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Shop extends React.PureComponent {
+
+    componentDidMount () {
+    }
+    render() {
+        return (
+            <div>
+                <p>Shop</p>
+            </div>
+        )
+    }
+}
+
+export default Shop
