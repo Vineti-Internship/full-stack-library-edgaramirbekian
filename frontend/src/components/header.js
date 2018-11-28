040fd7a3f1e3360a7085cc5ceb3e078a0cdefca9
Edgar <amirbekianedgar@gmail.com>
2018-11-28 11:36:49 +0400

    add Header to page

---
+++ frontend/src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Header extends React.PureComponent {
+    render() {
+        return (
+            <div className = 'Card'>
+                <p>Book Store Demo</p>
+            </div>
+        )
+    }
+}
+
+export default Header
