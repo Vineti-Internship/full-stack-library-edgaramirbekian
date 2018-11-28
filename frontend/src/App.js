21e72a9ba2fff277505cb20b1ada88a446881d72
Edgar <amirbekianedgar@gmail.com>
2018-11-28 16:58:18 +0400

    add some design to header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,23 @@ import './App.css';
 import './components/header'
 import Header from './components/header';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Footer from './components/footer';
+import Shop from './components/shop';
 
 
 class App extends Component {
   render() {
     return (
-      <div className = "jumbotron">
-        <Header />
+      <div>
+        <div className = "jumbotron">
+          <Header />
+        </div>
+        <div>
+          <Shop />
+        </div>
+        <div className = 'jumbotron'>
+          <Footer />
+        </div>
       </div>
     );
   }
--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -3,8 +3,8 @@ import React from 'react'
 class Header extends React.PureComponent {
     render() {
         return (
-            <div className = 'Card'>
-                <p>Book Store Demo</p>
+            <div>
+                <h3>Book Store Demo</h3>
             </div>
         )
     }
