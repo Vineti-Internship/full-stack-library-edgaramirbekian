b2301d64256c806442c2314900c1f4db5c210870
Edgar <amirbekianedgar@gmail.com>
2018-11-29 10:36:00 +0400

    add session routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :writers
   resources :books
   resources :writers
+  resources :sessions, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
