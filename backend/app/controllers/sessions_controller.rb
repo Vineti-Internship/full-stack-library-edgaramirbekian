09beb634aa5c6fbfa168a6ab894aa05b60a7687c
Edgar <amirbekianedgar@gmail.com>
2018-11-29 19:09:27 +0400

    add session controller validation functionality at POST requests

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -2,7 +2,14 @@ class SessionsControler < ApplicationController
 
     # POST /sessions
     def create
-        
+        @writer = Writer.where(email: params[:email]).first
+
+        if @writer.valid_password(params[:password])
+            render json: writer
+        else
+            head(:unauthorized)
+        end
+
     end
 
     # DELETE /sessions/id
