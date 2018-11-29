6c11f9dc6e24f8f6f1814e1631fdb76735f76f3d
Edgar <amirbekianedgar@gmail.com>
2018-11-29 23:01:22 +0400

    add token to writer model

--- backend/app/models/writer.rb
+++ backend/app/models/writer.rb
@@ -1,6 +1,7 @@
 class Writer < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    acts_as_token_authenticatable
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
     devise :database_authenticatable, :registerable,
             :recoverable, :rememberable, :validatable
     has_many :books
