47d2fa7d32063b13f9508e726aac194bd83271d3
Edgar <amirbekianedgar@gmail.com>
2018-12-02 01:18:21 +0400

    add writer deletion functionality (same problem with input->state existing)

---
+++ frontend/src/components/delwriter.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export class DelWriter extends React.Component {
+    constructor () {
+        super ();
+        this.state = {
+            writer_id: ''
+        }
+        this.idhandler = this.idhandler.bind(this)
+
+    }
+
+    async delete_writer () {
+        fetch('https://localhost:3000/writers'+this.state.writer_id, {method: 'DELETE'})
+    }
+
+    idhandler (event) {
+        this.setState({writer_id: event.target.value});
+    }
+
+    render () {
+        return (
+            <form>
+                <label>
+                    Book ID:
+                    <input type="text" name="writer_id" value={this.state.writer_id} onChange = {this.idhandler}/>
+                </label>
+                <input type="submit" value="Submit" onClick= {
+                    this.delete_writer
+                    }/>
+            </form>
+        )
+    }
+}
