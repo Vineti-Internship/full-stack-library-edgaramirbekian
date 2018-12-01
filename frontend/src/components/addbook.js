4ad11b46ecb80cfea6b40a23743090f40bbc4d6b
Edgar <amirbekianedgar@gmail.com>
2018-12-01 23:46:09 +0400

    add writer and book creation functionality (implementation)

---
+++ frontend/src/components/addbook.js
@@ -0,0 +1,67 @@
+import React from 'react'
+
+export class AddBook extends React.Component {
+    constructor () {
+        super ();
+        this.state = {
+            title: '',
+            category: '',
+            rating: 0,
+            writer_id: 0
+        }
+        this.post_request = this.post_request.bind(this);
+    }
+
+    async post_request () {
+        fetch('https://localhost:3000/books', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify ({
+            title: this.state.title,
+            category: this.state.category,
+            rating: this.state.rating,
+            writer_id: this.state.writer_id
+            })
+        })
+    }
+
+    render () {
+        return (
+            <form>
+                <label>
+                    Title:
+                    <input type="text" name="title" value={this.state.title} onChange = {
+                        (event) => {this.setState({title: event.target.value})}
+                        }/>
+                </label>
+                <label>
+                    Category:
+                    <input type="text" name="category" value={this.state.category} onChange = {
+                        (event) => {this.setState({category: event.target.value})}
+                        }/>
+                </label>
+                <label>
+                    Rating:
+                    <input type="text" name="rating" value={this.state.rating} onChange = {
+                        (event) => {this.setState({rating: parseInt(event.target.value)})}
+                        }/>
+                </label>
+                <label>
+                    Writer ID:
+                    <input type="text" name="writer_id" value={this.state.writer_id} onChange = {
+                        (event) => {this.setState({writer_id: parseInt(event.target.value)})}
+                    }/>
+                </label>
+                <input type="submit" value="Submit" onClick= {
+                        // this.post_request
+                        console.log(this.state)
+                    }/>
+            </form>
+        )
+    }
+
+
+}
---
+++ frontend/src/components/addwriter.js
@@ -0,0 +1,68 @@
+import React from 'react'
+
+export class AddWriter extends React.Component {
+    constructor () {
+        super ();
+        this.state = {
+            first_name: '',
+            last_name: '',
+            password: '',
+            email: ''
+        }
+        this.post_request = this.post_request.bind(this)
+
+    }
+
+    async post_request () {
+        fetch('https://localhost:3000/writers', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify ({
+            first_name: this.state.first_name,
+            last_name: this.state.last_name,
+            password: this.state.password,
+            email: this.state.email
+            })
+        })
+    }
+
+    render () {
+        return (
+            <form>
+                <label>
+                    First Name:
+                    <input type="text" name="first_name" value={this.state.first_name} onChange = {
+                        (event) => {this.setState({first_name: event.target.value})}
+                        }/>
+                </label>
+                <label>
+                    Last Name:
+                    <input type="text" name="last_name" value={this.state.last_name} onChange = {
+                        (event) => {this.setState({last_name: event.target.value})}
+                        }/>
+                </label>
+                <label>
+                    Password:
+                    <input type="pasword" name="password" value={this.state.password} onChange = {
+                        (event) => {this.setState({password: event.target.value})}
+                        }/>
+                </label>
+                <label>
+                    Email:
+                    <input type="text" name="email" value={this.state.email} onChange = {
+                        (event) => {this.setState({email: event.target.value})}
+                    }/>
+                </label>
+                <input type="submit" value="Submit" onClick= {
+                    // this.post_request
+                    console.log(this.state)
+                    }/>
+            </form>
+        )
+    }
+
+
+}
